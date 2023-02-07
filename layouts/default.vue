822bea1da35d82e5a2273e8855435e60c66c8b6a
Jeevana V <Jeevana.V@mrcooper.com>
2023-02-07 17:05:03 +0530

    Added side-bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,44 @@
 <template>
-    <div>
-        <Head>
-            <Title>Naan Mudhalvan Massive Upskilling Platform</Title>
-        </Head>
-        <v-app>
-            <v-app-bar dense elevation="4" flat outlined prominent rounded></v-app-bar>
-            <br/><br/><br/>
-            <div>
-                <slot/>
-            </div>
-        </v-app>
-    </div>
+  <div>
+    <Head>
+      <Title>Naan Mudhalvan Massive Upskilling Platform</Title>
+    </Head>
+    <v-app style="position: fixed">
+      <v-app-bar
+        dense
+        elevation="4"
+        flat
+        outlined
+        prominent
+        rounded
+      ></v-app-bar>
+      <div>
+        <slot />
+      </div>
+
+      <v-card>
+        <v-layout>
+          <v-navigation-drawer  permanent class="nav-drawer" >
+            
+            <v-list>
+              <v-list-item title="Home" value="home"></v-list-item>
+              <v-list-item title="Jobs" value="Jobs"></v-list-item>
+              <v-list-item title="My Profile" value="My Profile"></v-list-item>
+            </v-list>
+          </v-navigation-drawer>
+
+          <v-main style="height: 100vh; position: relative"></v-main>
+        </v-layout>
+      </v-card>
+    </v-app>
+  </div>
 </template>
-    
+
+<style>
+.nav-drawer{
+background: #304443;
+position:fixed; 
+margin-top: 2em; 
+padding-top: 2rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,6 @@
         <Head>
         <Title>Naan Mudhalvan Massive Upskilling Platform</Title>
         </Head>
-        <h1>Hello World</h1>
     </div>
 </template>
 <script setup>
