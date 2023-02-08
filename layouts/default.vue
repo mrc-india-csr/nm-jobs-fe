144b5710e7394ee79ce00e9daa9f0918b884bc9e
Hariananthan K P <hariananthan93@gmail.com>
2023-02-08 10:14:01 +0530

    Added border for dropdown

--- layouts/default.vue
+++ layouts/default.vue
@@ -26,7 +26,7 @@
   }
 </script>
 
-<style scoped>
+<style>
 .logo{
     margin: 20px;
     padding : 20px;
@@ -36,4 +36,9 @@
     margin-top: 20px;
     max-width: 150px;
 }
+
+.language.v-select>.v-input__control{ 
+     border: 1px solid rgba(0, 0, 0, 0.38)!important;
+} 
+
 </style>
