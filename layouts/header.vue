b881f0c2841deb4151f0fb26dfd4c8d31c5f2f23
Varun Rishwandh S <VarunRishwandh.S@mrcooper.com>
2023-02-08 13:01:41 +0530

    added sidebars

--- layouts/header.vue
+++ layouts/header.vue
@@ -1,22 +1,29 @@
 <template>
-    <div>
-        <Head>
-            <Title>Naan Mudhalvan Massive Upskilling Platform</Title>
-        </Head>
-        
-        <v-app>
-            <v-app-bar dense elevation="4" flat outlined prominent rounded></v-app-bar>
-            <br/><br/><br/>
-            <div>
-                <slot/>
-            </div>
-   </v-app>
-    </div>
+  <div>
+    <Head>
+      <Title>Naan Mudhalvan Massive Upskilling Platform</Title>
+    </Head>
+
+    <v-app>
+      <v-app-bar
+        dense
+        elevation="4"
+        flat
+        outlined
+        prominent
+        rounded
+      ></v-app-bar>
+      
+      <div>
+        <slot />
+      </div>
+    </v-app>
+  </div>
 </template>
 <script>
-export default{
-    name:'headers'
-}
+export default {
+  name: "headers",
+};
 </script>
 
     
--- layouts/sidenav.vue
+++ layouts/sidenav.vue
@@ -1,53 +1,73 @@
 <template>
-    <v-card>
-        <v-layout>
-          <v-navigation-drawer permanent class="nav-drawer sidebar-font" style="top:66px;  background: #304443">
-            <v-list>
-              <v-list-item
-                title="Home"
-                class="sidebar-font"
-                value="home"
-              ></v-list-item>
-              <v-list-item
-                title="Jobs"
-                class="sidebar-font"
-                value="Jobs"
-              ></v-list-item>
-              <v-list-item
-                title="My Profile"
-                class="sidebar-font"
-                value="My Profile"
-              ></v-list-item>
-              
-            </v-list>
-          </v-navigation-drawer>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer permanent class="nav-drawer sidebar-font">
+        <v-list
+        >
+          <v-list-item
+          @click="handleClick"
+            title="Home"
+            class="sidebar-font"
+            value="home"
+          ></v-list-item>
+          <v-list-item
+          @click="handleClick"
+            title="Jobs"
+            class="sidebar-font"
+            value="Jobs"
+          ></v-list-item>
+          <v-list-item
+          @click="handleClick"
+            title="My Profile"
+            class="sidebar-font"
+            value="My Profile"
+          ></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
 
-          <v-main style="height: 100vh; position: relative"></v-main>
-        </v-layout>
+      <v-main style="height: 100vh; position: relative"></v-main>
+    </v-layout>
   </v-card>
 </template>
-<style scoped>
+<style >
 .nav-drawer {
-  min-width: 8.875rem;
+  max-width: 8.875rem;
   background: #304443;
   position: fixed;
-  margin-top: 76px;
-  padding-top: 2.5rem;
+  margin-top: 3.8rem;
 }
 
 .sidebar-font {
- 
-font-family: 'Roboto';
-font-style: normal;
-font-weight: 500;
-font-size: 16px;
-line-height: 19px;
-
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  color: white;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
 
 }
+.sidebar-font
+  :active {
+  color: #ffe5c7;
+  background: #253534;}
+  
 </style>
 <script>
-export default{
-    name:'sidenav'
-}
+export default {
+  name: "sidenav",
+
+  data() {
+    return {
+      active: false,
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log("Here")
+      this.active = !this.active;
+      console.log(this.active)
+    },
+  },
+};
 </script>
